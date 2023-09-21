3bddae73eeffbf917915ce84c45b3fc9bd6b56c9
meaninglessKyle <kyle9202@163.com>
2023-09-21 23:24:48 +0800

    111

---
+++ index.js
@@ -0,0 +1,20 @@
+import {bbb} from './bbb.js';
+
+console.log(111);
+console.log(bbb);
+
+/**
+ * 入参均为 0 - 1
+ * 输出 0 - 1
+ * */
+const cubic = Object.freeze({
+    in: amount => {
+        return Math.pow(amount, 3);
+    },
+    out: amount => {
+        return Math.pow(1 - amount, 3) + 1;
+    },
+    inOut: amount  => {
+
+    }
+})
